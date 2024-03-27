4085f03db74bdff42432a4d4f240e7e22b5aab42
MingchuanTang <1418767078@qq.com>
2024-03-27 22:00:34 +0800

    update

--- lib/database_interaction.py
+++ lib/database_interaction.py
@@ -56,7 +56,7 @@ import requests
 
 
 # Ensembl_database
-def ensembl_name_to_seqs(gene="BRCA1", species="human", seq_type="cds"):
+def ensembl_name_to_seqs(gene="BRCA1", species="human", seq_type="cds", tqdm_args={'position': 0, 'leave': True}):
     lookup_url = f"http://rest.ensembl.org/lookup/symbol/{species}/{gene}?content-type=application/json"
     gene_id = requests.get(url=lookup_url).json()["id"]
 
@@ -64,18 +64,18 @@ def ensembl_name_to_seqs(gene="BRCA1", species="human", seq_type="cds"):
     transcripts = requests.get(url=transcripts_url).json()
 
     # Get sequences for each transcript
-    sequences = {}
-    for transcript in tqdm(transcripts, desc=f"Gene:\t{gene}"):
-        try:
-            seq_name = "|".join(
-                [transcript["id"], transcript["external_name"], transcript["biotype"]]
-            )
-            seq_url = f"http://rest.ensembl.org/sequence/id/{transcript['id']}?type={seq_type};content-type=application/json"
-            seq_response = requests.get(seq_url).json()
-            sequences[seq_name] = seq_response["seq"]
-        except:
-            continue
-
+    sequences = []
+    with tqdm(total=len(transcripts), desc=f"{gene}", **tqdm_args) as pbar_task:
+        for transcript in transcripts:
+            try:
+                seq_url = f"http://rest.ensembl.org/sequence/id/{transcript['id']}?type={seq_type};content-type=application/json"
+                seq_response = requests.get(seq_url).json()
+                transcript["seq"] = seq_response["seq"]
+                sequences.append(transcript)
+                pbar_task.update(1)
+            except:
+                pbar_task.update(1)
+                continue
     return sequences
 
 def ensembl_id_to_seqs(gene="Gm16024", gene_id='ENSMUST00000128841.1', seq_type="cds"):
@@ -83,15 +83,13 @@ def ensembl_id_to_seqs(gene="Gm16024", gene_id='ENSMUST00000128841.1', seq_type=
     transcripts = requests.get(url=transcripts_url).json()
 
     # Get sequences for each transcript
-    sequences = {}
+    sequences = []
     for transcript in tqdm(transcripts, desc=f"Gene:\t{gene}"):
         try:
-            seq_name = "|".join(
-                [transcript["id"], transcript["external_name"], transcript["biotype"]]
-            )
             seq_url = f"http://rest.ensembl.org/sequence/id/{transcript['id']}?type={seq_type};content-type=application/json"
             seq_response = requests.get(seq_url).json()
-            sequences[seq_name] = seq_response["seq"]
+            transcript["seq"] = seq_response["seq"]
+            sequences.append(transcript)
         except:
             continue
 
